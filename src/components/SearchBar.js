38639727483a136029c3f6cd8d115b0bb2ff6a55
Kris Kewish <kristopherkewish@gmail.com>
2023-11-27 17:44:31 +1100

    Add state hook to search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import styles from './SearchBar.module.css'
 
-const SearchBar = () => {
+const SearchBar = (props) => {
   return (
     <div className={styles.searchContainer}>
-      <input className={styles.searchBar} type="text" />
+      <input className={styles.searchBar} type="text" value={props.searchText} onChange={props.handleChange}/>
       <button className={styles.searchBtn}>Search</button>
     </div>
   );
--- src/containers/TunaPlaylistContainer.js
+++ src/containers/TunaPlaylistContainer.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Playlist from '../components/Playlist';
 import SearchBar from '../components/SearchBar';
 import SearchResults from '../components/SearchResults';
 import styles from './TunaPlaylistContainer.module.css'
 
 const TunaPlaylistContainer = () => {
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearchChange = ({target}) => {
+    setSearchText(target.value);
+  };
+
   return (
     <div>
-      <div className={styles.searchBar}><SearchBar /></div>
+      <div className={styles.searchBar}><SearchBar value={searchText} handleChange={handleSearchChange}/></div>
       <div className={styles.container}>
         <div className={styles.searchResults}><SearchResults /></div>
         <div className={styles.playlist}><Playlist  /></div>
       </div>
-      
     </div>
   );
 };
